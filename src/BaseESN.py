fixed the FESN and added a sinewave generator and simple sinewave sample for it. implemented a training error calculation for ESN and FESN.

src/BaseESN.py
@@ -82,14 +82,14 @@ class BaseESN:
         self._W_input = self._W_input.dot(self._expanded_input_scaling_matrix)
 
         if (feedback):
-            self._W_feedback = np.random.rand(self.n_reservoir, self.n_output) - 0.5
+            self._W_feedback = np.random.rand(self.n_reservoir, 1+self.n_output) - 0.5
 
     def update(self, inputData):
         """
         returns the UNSCALED but reshaped input of this step
         """
         u = inputData.reshape(self.n_input, 1)
-        self._x = (1.0-self.leak_rate)*self._x + self.leak_rate*np.arctan(np.dot(self._W_input, np.vstack((self.bias, u))) + np.dot(self._W, self._x)) + (np.random.rand()-0.5)*self.noise_level
+        self._x = (1.0-self.leak_rate)*self._x + self.leak_rate*np.arctan(np.dot(self._W_input, np.vstack((self.bias, u))) + np.dot(self._W, self._x) + (np.random.rand()-0.5)*self.noise_level)
 
         return u
 
@@ -97,18 +97,20 @@ class BaseESN:
         """
         returns the UNSCALED but reshaped input of this step
         """
-
         #the input is allowed to be "empty" (size=0)
         if (self.n_input != 0):
             u = inputData.reshape(self.n_input, 1)
             outputData = outputData.reshape(self.n_output, 1)
             #TODO: Fix the brackets arround the noise!
-            self._x = (1.0-self.leak_rate)*self._x + self.leak_rate*np.arctan(np.dot(self._W_input, np.vstack((self.bias, u))) + np.dot(self._W, self._x) + np.dot(self._W_feedback, outputData)) + (np.random.rand()-0.5)*self.noise_level
+            self._x = (1.0-self.leak_rate)*self._x + self.leak_rate*np.arctan(np.dot(self._W_input, np.vstack((self.bias, u))) + np.dot(self._W, self._x) +
+                np.dot(self._W_feedback, np.vstack((self.output_bias, outputData))) + (np.random.rand()-0.5)*self.noise_level)
 
             return u
         else:
             outputData = outputData.reshape(self.n_output, 1)
-            self._x = (1.0-self.leak_rate)*self._x + self.leak_rate*np.arctan(np.dot(self._W, self._x) + np.dot(self._W_feedback, outputData) + (np.random.rand()-0.5)*self.noise_level)
+            self._x = (1.0-self.leak_rate)*self._x + self.leak_rate*np.arctan(np.dot(self._W, self._x) + np.dot(self._W_feedback, np.vstack((self.output_bias, outputData))) +
+                (np.random.rand()-0.5)*self.noise_level)
+
             return np.empty((0,1))
 
     def save(self, path):

src/ESN.py
@@ -17,9 +17,10 @@ class ESN(BaseESN):
         if (inputData.shape[0] != outputData.shape[0]):
             raise ValueError("Amount of input and output datasets is not equal - {0} != {1}".format(inputData.shape[0], outputData.shape[0]))
 
-        trainLength = inputData.shape[0]-1
-        skipLength = int(trainLength*transient_quota)
+        trainLength = inputData.shape[0]
 
+        skipLength = int(trainLength*transient_quota)
+    
         #define states' matrix
         self._X = np.zeros((1+self.n_input+self.n_reservoir,trainLength-skipLength))
 
@@ -33,13 +34,19 @@ class ESN(BaseESN):
 
         #define the target values
         #                                  +1
-        Y_target = self.out_inverse_activation(outputData).T[:,skipLength+1:trainLength+1]
+        Y_target = self.out_inverse_activation(outputData).T[:,skipLength:]
 
         #W_out = Y_target.dot(X.T).dot(np.linalg.inv(X.dot(X.T) + regressionParameter*np.identity(1+reservoirInputCount+reservoirSize)) )
         if (regression_parameter is None):
             self._W_out = np.dot(Y_target, np.linalg.pinv(self._X))
         else:
             self._W_out = np.dot(np.dot(Y_target, self._X.T),np.linalg.inv(np.dot(self._X,self._X.T) + regression_parameter*np.identity(1+self.n_input+self.n_reservoir)))
+
+        #calculate the training error now
+        train_prediction = np.dot(self._W_out, self._X).T
+        training_error = np.sqrt(np.mean((train_prediction - outputData[skipLength:])**2))
+
+        return training_error
 
     def generate(self, n, initial_input, continuation=True, initial_data=None, update_processor=lambda x:x):
         if (self.n_input != self.n_output):

src/FESN.py
@@ -21,7 +21,7 @@ class FESN(BaseESN):
         else:
             inputData = np.empty((len(outputData), 0))
 
-        trainLength = outputData.shape[0]-1
+        trainLength = outputData.shape[0]
         skipLength = int(trainLength*transient_quota)
 
         #define states' matrix
@@ -39,14 +39,20 @@ class FESN(BaseESN):
             oldOutput = outputData[t]
 
         #define the target values
-        #                                  +1
-        Y_target = self.out_inverse_activation(outputData).T[:,skipLength+1:trainLength+1]
+        #                                                               +1
+        Y_target = self.out_inverse_activation(outputData).T[:,skipLength:trainLength]
 
         #W_out = Y_target.dot(X.T).dot(np.linalg.inv(X.dot(X.T) + regressionParameter*np.identity(1+reservoirInputCount+reservoirSize)) )
         if (regression_parameter is None):
             self._W_out = np.dot(Y_target, np.linalg.pinv(self._X))
         else:
             self._W_out = np.dot(np.dot(Y_target, self._X.T),np.linalg.inv(np.dot(self._X,self._X.T) + regression_parameter*np.identity(1+self.n_input+self.n_reservoir)))
+
+        #calculate the training error now
+        train_prediction = np.dot(self._W_out, self._X).T
+        training_error = np.sqrt(np.mean((train_prediction - outputData[skipLength:])**2))
+
+        return training_error
 
     def predict(self, inputData, n=None, continuation=True, initial_input_data=None, initial_output_data=None, start_output = None, update_processor=lambda x:x):
         if (self.n_input == 0):

src/test/esn_test2.py
@@ -20,7 +20,9 @@ def sineshit():
     y = (0*np.log(x)+np.sin(x)*np.cos(x)).reshape(20000,1)*2
 
     esn = ESN(n_input=1, n_output=1, n_reservoir=200, random_seed=42, noise_level=0.001, leak_rate=0.7, spectral_radius=1.35, sparseness=0.1)
-    esn.fit(inputData=y[:5000, :], outputData=y[:5000,:], transient_quota=0.4, regression_parameter=2e-4)
+    train_error = esn.fit(inputData=y[:5000, :], outputData=y[1:5001,:], transient_quota=0.4, regression_parameter=2e-4)
+    print("train error: {0:4f}".format(train_error))
+    print(np.mean(np.abs(esn._W_out)))
 
     Y = esn.generate(n=15000, continuation=True, initial_input=y[5000,:])
 
@@ -31,7 +33,7 @@ def sineshit():
     #plt.plot(x[5000:],Y[0,:]-y[5000:,0], linestyle=":")
     plt.show()
 
-#reset the reservoir after each series!
+sineshit()
 
 def read_data(path, sizeString):
     content = ""
@@ -75,7 +77,7 @@ def read_data(path, sizeString):
 trainX, trainY = read_data("ae.train", "30 30 30 30 30 30 30 30 30")
 testX, testY = read_data("ae.test", "31 35 88 44 29 24 40 50 29")
 
-esn = CESN(n_input=12, n_output=9, n_reservoir=1000, leak_rate=0.8, spectral_radius=0.2, random_seed=41, noise_level=0.01, sparseness=0.30)
+esn = CESN(n_input=12, n_output=9, n_reservoir=1000, leak_rate=0.8, spectral_radius=0.2, random_seed=41, noise_level=0.1, sparseness=0.30)
 esn.fit(inputList=trainX, outputList=trainY)
 
 err = 0.0

src/test/esn_test4.py
@@ -3,47 +3,120 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-# Code for the Sinewave generator
+# Code for the Sinewave generator & a simple sine wave generator
 # first test for the feedback ESN (FESN)
 # is supposed to generate autonomously a sinewave
 # input: target frequency
-
 
 from FESN import FESN
 import numpy as np
 import matplotlib.pyplot as plt
 
 def simple_sine():
-    generationLength = 20000
-    trainLength = 5000
-    testLength = generationLength - trainLength
+    x = np.linspace(1,100*np.pi, 2000)
+    y = (0*np.log(x)+np.sin(x)*np.cos(x)).reshape(2000,1)*2
 
-    x = np.linspace(0, 200*np.pi, generationLength)
-    y = (np.sin(x)).reshape(generationLength,1)*0.2+0.5
+    #esn = ESN(n_input=1, n_output=1, n_reservoir=200, random_seed=42, noise_level=0.001, leak_rate=0.7, spectral_radius=1.35, sparseness=0.1)
+    #esn.fit(inputData=y[:5000, :], outputData=y[:5000,:], transient_quota=0.4, regression_parameter=2e-4)
 
-    #x = np.linspace(1,200*np.pi, 20000)
-    #y = (0*np.log(x)+np.sin(x)*np.cos(x)).reshape(20000,1)*2
-
-
-    plt.plot(x,y)
-    #plt.show()
-
-    esn = FESN(n_input=0, n_output=1, output_bias=1.0, n_reservoir=400, random_seed=42, noise_level=0.0001, leak_rate=0.70,
-        spectral_radius=0.9, weight_generation="advanced", sparseness=0.2)
-    esn.fit(inputData=None, outputData=y[1:trainLength,:], transient_quota=0.1)
+    esn = FESN(n_input=0, n_output=1, output_bias=1.0, n_reservoir=400, random_seed=42, noise_level=0.001, leak_rate=0.70,
+        spectral_radius=1.35, sparseness=0.1)
+    esn.fit(inputData=None, outputData=y[0:500,:], transient_quota=0.4)
 
     print(np.mean(np.abs(esn._W_out)))
 
     #Y = esn.predict(n=testLength, inputData=None, continuation=True, start_output = y[trainLength-1])
-    Y = esn.predict(n=testLength, inputData=None, continuation=True, start_output = y[trainLength])
-
-    Y = Y.T
-
-    print(Y.shape)
-    print(x[:trainLength].shape)
+    Y = esn.predict(n=1500, inputData=None, continuation=True, start_output = y[500,:])
 
     #plt.plot(np.arange(trainLength), Y, 'r')
-    plt.plot(x[trainLength:], Y, "r")
+
+    plt.plot(x,y[:,0], "b", linestyle="--")
+    plt.plot(x[500:],Y[0,:], "r")
     plt.show()
 
-simple_sine()
+#simple_sine()
+
+
+def frequency_generator(N,min_period,max_period,n_changepoints):
+    """returns a random step function with N changepoints
+       and a sine wave signal that changes its frequency at
+       each such step, in the limits given by min_ and max_period."""
+    # vector of random indices < N, padded with 0 and N at the ends:
+    changepoints = np.insert(np.sort(np.random.randint(0,N,n_changepoints)),[0,n_changepoints],[0,N])
+    # list of interval boundaries between which the control sequence should be constant:
+    const_intervals = list(zip(changepoints,np.roll(changepoints,-1)))[:-1]
+    # populate a control sequence
+    frequency_control = np.zeros((N,1))
+    for (t0,t1) in const_intervals:
+        frequency_control[t0:t1] = np.random.rand()
+
+    frequency_control[const_intervals[0][0]:const_intervals[0][1]] = 0.8
+    frequency_control[const_intervals[len(const_intervals)-1][0]:const_intervals[len(const_intervals)-1][1]] = 0.2
+
+
+    periods = frequency_control * (max_period - min_period) + max_period
+    # run time through a sine, while changing the period length
+    frequency_output = np.zeros((N,1))
+    z = 0
+    for i in range(N):
+        z = z + 2 * np.pi / periods[i]
+        frequency_output[i] = (np.sin(z) + 1)/2
+    return np.hstack([np.ones((N,1)),1-frequency_control]), frequency_output
+
+
+
+
+def freq_gen():
+    np.random.seed(42)
+
+    N = 20000 # signal length
+    min_period = 2
+    max_period = 10
+    n_changepoints = int(N/200)
+    frequency_control,frequency_output = frequency_generator(N,min_period,max_period,n_changepoints)
+
+    frequency_control2, frequency_output2 = frequency_generator(N,min_period,max_period,n_changepoints)
+
+    traintest_cutoff = int(np.ceil(0.5*N))
+
+    train_ctrl,train_output = frequency_control[:traintest_cutoff],frequency_output[:traintest_cutoff]
+    test_ctrl, test_output  = frequency_control[traintest_cutoff:],frequency_output[traintest_cutoff:]
+
+
+    #esn = FESN(n_input=1, n_output=1, output_bias=0.0, n_reservoir=200, output_input_scaling=1.0, random_seed=42, noise_level=0.001, leak_rate=0.6,
+    #    spectral_radius=0.2, sparseness=0.30)
+
+    #for random see 42 for the generation!
+    esn = FESN(n_input=1, n_output=1, output_bias=0.0, n_reservoir=200, output_input_scaling=1.0, random_seed=42, noise_level=0.001, leak_rate=0.7,
+        spectral_radius=0.13, sparseness=0.30)
+
+    esn.fit(train_ctrl[:,1], train_output, transient_quota=0.1)
+    pred_test = esn.predict(test_ctrl[:,1]).T
+    pred_test2 = esn.predict(frequency_control2[:,1]).T
+
+    #test acc
+    print(np.sqrt(np.mean((pred_test - test_output)**2)))
+    #second test acc for validation with completly new generated samples
+    print(np.sqrt(np.mean((pred_test2 - frequency_output2)**2)))
+
+    #window_test = range(2000)
+    plt.figure(figsize=(10,2.5))
+    plt.plot(test_ctrl[-2000:,1],label='control')
+    plt.plot(test_output[-2000:],label='target')
+    plt.plot(pred_test[-2000:],label='model')
+    plt.legend(fontsize='x-small')
+    plt.title('test (excerpt)')
+    plt.ylim([-1.5,1.5]);
+
+
+    plt.figure(figsize=(10,2.5))
+    plt.plot(frequency_control2[:,1],label='control')
+    plt.plot(frequency_output2[:],label='target')
+    plt.plot(pred_test2[:],label='model')
+    plt.legend(fontsize='x-small')
+    plt.title('test2 (excerpt)')
+    plt.ylim([-1.5,1.5]);
+
+    plt.show()
+
+freq_gen()