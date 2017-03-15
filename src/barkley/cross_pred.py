d0ed5de68f1f3a85805821622662a845fd01d55a
FlashTek <rzrolandzimmermann@live.de>
2017-03-15 18:11:00 +0100

    feature: added method to create patches for the input/output of the image

--- src/barkley/cross_pred.py
+++ src/barkley/cross_pred.py
@@ -32,132 +32,151 @@ def generate_data(N, trans, sample_rate=1):
 
     return data
 
+def create_patch_indices(outer_range_x, outer_range_y, inner_range_x, inner_range_y):
+    outer_ind_x = np.tile(range(outer_range_x[0], outer_range_x[1]+1), outer_range_y[1])
+    outer_ind_y = np.repeat(range(outer_range_y[0], outer_range_y[1]+1), outer_range_x[1])
+
+    inner_ind_x = np.tile(range(inner_range_x[0], inner_range_x[1]+1), inner_range_y[1]-1)
+    inner_ind_y = np.repeat(range(inner_range_y[0], inner_range_y[1]+1), inner_range_x[1]-1)
+
+    outer_list = [c for c in zip(outer_ind_y, outer_ind_x)]
+    inner_list = [c for c in zip(inner_ind_y, inner_ind_x)]
+
+    real_list = np.array([x for x in outer_list if x not in inner_list])
+    inner_list = np.array(inner_list)
+
+    return real_list[:,0], real_list[:,1], inner_list[:, 0], inner_list[:, 1]
+
+def print_field(input_y, input_x, output_y, output_x):
+    print_matrix = np.zeros((30,30))
+    print_matrix[input_y, input_x] = 1.0
+    print_matrix[output_y, output_x] = -1.0
+    for y in range(30):
+        string = "|"
+        for x in range(30):
+            if (print_matrix[y,x] == 1.0):
+                string += "x"
+            elif (print_matrix[y,x] == -1.0):
+                string += "0"
+            else:
+                string += "."
+
+        string += "|"
+        print(string)
+
+
+
 print("generating data...")
-data = generate_data(10000, 50000, 5)
-np.save("10000.dat", data)
+#data = generate_data(10000, 50000, 5)
+#np.save("10000.dat", data)
 data = np.load("10000.dat.npy")
-
-"""
-i = 0
-def update_new(d):
-    global i
-    mat.set_data(data[i,:].reshape((30, 30)))
-    plt.title(i)
-
-    i = (i+1) % 500
-    return [mat]
-
-fig, ax = plt.subplots()
-
-mat = ax.matshow(data[0,:].reshape((30, 30)), vmin=0, vmax=1, interpolation=None, origin="lower")
-plt.colorbar(mat)
-ani = animation.FuncAnimation(fig, update_new, interval=0, save_count=50)
-plt.show()
-"""
-
-#data = data.reshape((1000, -1))
 
 training_data = data[:4000]
 test_data = data[4000:]
 
-#out_ind = [14,15,16]
-out_ind_x = [8,9,10,11,12,13]#[14,15,16]#[8,9,10]
-out_ind_y = [8,9,10,11,12,13]#[14,15,16]#[8,9,10]
-#in_ind = list(range(14))
-#in_ind.extend(list(range(17,30)))
-in_ind_x = list(range(0, 30))
-in_ind_y = list(range(0, 30))
-for i in out_ind_x:
-    in_ind_x.remove(i)
-for i in out_ind_y:
-    in_ind_y.remove(i)
+#input_y, input_x, output_y, output_x = create_patch_indices((12,17), (12,17), (13,16), (13,16)) # -> yields MSE=0.0115 with leak_rate = 0.8
+input_y, input_x, output_y, output_x = create_patch_indices((4,23), (4,23), (7,20), (7,20)) # -> yields MSE=0.0873 with leak_rate = 0.3
 
-training_data_in =  training_data[:, in_ind_y][:,:, in_ind_x].reshape(-1, len(in_ind_x)*len(in_ind_y))
-training_data_out =  training_data[:, out_ind_y][:,:, out_ind_x].reshape(-1, len(out_ind_x)*len(out_ind_y))
+training_data_in =  training_data[:, input_y, input_x].reshape(-1, len(input_y))
+training_data_out =  training_data[:, output_y, output_x].reshape(-1, len(output_y))
 
-test_data_in =  test_data[:, in_ind_y][:,:, in_ind_x].reshape(-1, len(in_ind_x)*len(in_ind_y))
-test_data_out =  test_data[:, out_ind_y][:,:, out_ind_x].reshape(-1, len(out_ind_x)*len(out_ind_y))
+test_data_in =  test_data[:, input_y, input_x].reshape(-1, len(input_y))
+test_data_out =  test_data[:, output_y, output_x].reshape(-1, len(output_y))
+
+
+generate_new = False
 
 print("setting up...")
+if (generate_new):
+    esn = ESN(n_input = len(input_y), n_output = len(output_y), n_reservoir = 1700,
+            weight_generation = "advanced", leak_rate = 0.8, spectral_radius = 0.8,
+            random_seed=42, noise_level=0.0001, sparseness=.1, regression_parameters=[6e-1], solver = "lsqr",
+            out_activation = lambda x: 0.5*(1+np.tanh(x/2)), out_inverse_activation = lambda x:2*np.arctanh(2*x-1))
 
-"""
-print("starting grid search...")
-from GridSearch import GridSearch
-grid = GridSearch(param_grid={"n_reservoir": [500, 700, 1000], "spectral_radius": [1.5, 1.7, 1.8, 1.9, 2.0, 2.1], "leak_rate": [.8, .95, .99], "sparseness": [0.05, 0.1, 0.2],
-                 "solver" : ["pinv", "lsqr"], "regression_parameters": [[2e-2], [2e-3], [2e-4]]},
-                fixed_params={"n_output": 3**2, "n_input": (30-3)**2, "noise_level": 0.001, "random_seed": 42, "weight_generation": "advanced",
-                "out_activation" : lambda x: 0.5*(1+np.tanh(x/2)), "out_inverse_activation" : lambda x:2*np.arctanh(2*x-1)},
-                esnType=ESN)
-print("start fitting...")
-results = grid.fit(training_data_in, training_data_out, [(test_data_in, test_data_out)])
-print("done:\r\n")
-print(results)
-print("best parameters: " + str(grid._best_params))
-print("best mse: {0}".format(grid._best_mse))
+    print("fitting...")
 
-#for the centre
-#best parameters: {'sparseness': 0.1, 'leak_rate': 0.95, 'spectral_radius': 1.5, 'n_reservoir': 500}
-#best mse: 0.028543328874922003
+    train_error = esn.fit(training_data_in, training_data_out,)
+    esn.save("esn" + str(len(input_y)) + ".dat")
+    print("train error: {0}".format(train_error))
 
-import sys
-sys.exit()
+else:
+    esn = ESN.load("esn" + str(len(input_y)) + ".dat")
 
-#best parameters: {'solver': 'lsqr', 'spectral_radius': 1.5, 'n_reservoir': 500, 'sparseness': 0.1, 'leak_rate': 0.8, 'regression_parameters': [0.02]}
-#best mse: 0.03164276788179946
-"""
-
-
-#for the lower left corner
-#best parameters: {'leak_rate': 0.99, 'sparseness': 0.1, 'spectral_radius': 2.1, 'solver': 'lsqr', 'n_reservoir': 1000, 'regression_parameters': [0.0002]}
-#best mse: 0.012300204613490656
-
-esn = ESN(n_input = len(in_ind_x)*len(in_ind_y), n_output = len(out_ind_x)*len(out_ind_y), n_reservoir = 2700,
-        weight_generation = "advanced", leak_rate = 0.7, spectral_radius = 0.8,
-        random_seed=42, noise_level=0.001, sparseness=.2, regression_parameters=[2e-2],
-        out_activation = lambda x: 0.5*(1+np.tanh(x/2)), out_inverse_activation = lambda x:2*np.arctanh(2*x-1))
-
-esn.save("esn.dat")
-
-#esn = ESN.load("esn.dat")
-esn.solver= "pinv"
-#esn._solver = "lsqr"
-#esn._regression_parameters = [2e-2]
-
-print("fitting...")
-train_error = esn.fit(training_data_in, training_data_out,)
-print("train error: {0}".format(train_error))
 print("predicting...")
 pred = esn.predict(test_data_in)
-pred = pred.reshape(-1, len(out_ind_x),len(out_ind_y))
 
 merged_prediction = test_data.copy()
-merged_prediction[np.ix_(list(range(len(test_data))), out_ind_x, out_ind_y)] = pred
+merged_prediction[:, output_y, output_x] = pred
 
-diff = pred.reshape((-1, len(out_ind_x)*len(out_ind_y)))-test_data_out
-mse = np.mean(diff.reshape(-1, len(out_ind_x)*len(out_ind_y))**2)
-print(mse)
-
-diff = np.abs(diff)
-
-
-#import sys
-#sys.exit()
+diff = pred.reshape((-1, len(output_y))) - test_data_out
+mse = np.mean((diff)**2)
+print("test error: {0}".format(mse))
 
 i = 0
 def update_new(data):
     global i
-    plt.title(i)
-    #mat.set_data(diff[i])
-    mat.set_data(merged_prediction[i])
-    i = (i+1) % len(diff)
+    if (not pause):
+        if (image_mode == 0):
+            mat.set_data(merged_prediction[i])
+        elif (image_mode == 1):
+                mat.set_data(test_data[i])
+        else:
+            mat.set_data(np.abs(test_data[i]-merged_prediction[i]))
+
+        i = (i+1) % len(diff)
+        sposition.set_val(i)
     return [mat]
 
 
 fig, ax = plt.subplots()
-#mat = plt.imshow(diff[0], origin="lower", vmin=np.min(diff), vmax=np.max(diff), interpolation="none", cmap=plt.get_cmap('gray'))
 mat = plt.imshow(merged_prediction[0], origin="lower", vmin=0, vmax=1, interpolation="none")
 plt.colorbar(mat)
+pause = False
+image_mode = 0
 ani = animation.FuncAnimation(fig, update_new, interval=1, save_count=50)
+
+from matplotlib.widgets import Button
+from matplotlib.widgets import Slider
+class UICallback(object):
+    def position_changed(self, value):
+        global i
+        value = int(value)
+        i = value
+
+    def playpause(self, event):
+        global pause, bplaypause
+        pause = not pause
+        bplaypause.label.set_text("Play" if pause else "Pause")
+
+    def switchsource(self, event):
+        global image_mode, bswitchsource
+        if (event.button == 1):
+            image_mode = (image_mode + 1) % 3
+        else:
+            image_mode = (image_mode - 1) % 3
+
+        if (image_mode == 0):
+            bswitchsource.label.set_text("Pred")
+        elif (image_mode == 1):
+            bswitchsource.label.set_text("Orig")
+        else:
+            bswitchsource.label.set_text("Diff")
+
+callback = UICallback()
+axplaypause = plt.axes([0.145, 0.91, 0.10, 0.05])
+axswitchsource = plt.axes([0.645, 0.91, 0.10, 0.05])
+axposition = plt.axes([0.275, 0.91, 0.30, 0.05])
+
+bplaypause = Button(axplaypause, "Pause")
+bplaypause.on_clicked(callback.playpause)
+
+bswitchsource = Button(axswitchsource, "Pred")
+bswitchsource.on_clicked(callback.switchsource)
+
+sposition = Slider(axposition, 'n', 0, len(test_data), valinit=0, valfmt='%1.0f')
+sposition.on_changed(callback.position_changed)
+
 plt.show()
+plt.close()
 
 print("done.")
