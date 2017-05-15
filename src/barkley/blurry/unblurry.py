0b016680d2482554f5c6682033f040069d6d15f8
FlashTek <rzrolandzimmermann@live.de>
2017-05-15 09:24:08 +0200

    .

--- src/barkley/blurry/unblurry.py
+++ src/barkley/blurry/unblurry.py
@@ -8,6 +8,7 @@ sys.path.insert(0, grandparentdir)
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from scipy.ndimage.filters import gaussian_filter
 import progressbar
 import dill as pickle
 
@@ -31,16 +32,16 @@ trainLength = ndata - testLength
 
 def setupArrays():
     #TODO: Correct the array dimensions!
-    global shared_training_data_base, shared_test_data_base, prediction_base
-    global shared_training_data, shared_test_data, prediction
+    global shared_input_data_base, shared_output_data_base, prediction_base
+    global shared_input_data, shared_output_data, prediction
 
-    shared_training_data_base = multiprocessing.Array(ctypes.c_double, 2*trainLength*N*N)
-    shared_training_data = np.ctypeslib.as_array(shared_training_data_base.get_obj())
-    shared_training_data = shared_training_data.reshape(2, trainLength, N, N)
+    shared_input_data_base = multiprocessing.Array(ctypes.c_double, 2*trainLength*N*N)
+    shared_input_data = np.ctypeslib.as_array(shared_input_data_base.get_obj())
+    shared_input_data = shared_input_data.reshape(2, trainLength, N, N)
 
-    shared_test_data_base = multiprocessing.Array(ctypes.c_double, 2*testLength*N*N)
-    shared_test_data = np.ctypeslib.as_array(shared_test_data_base.get_obj())
-    shared_test_data = shared_test_data.reshape(2, testLength, N, N)
+    shared_output_data_base = multiprocessing.Array(ctypes.c_double, 2*testLength*N*N)
+    shared_output_data = np.ctypeslib.as_array(shared_output_data_base.get_obj())
+    shared_output_data = shared_output_data.reshape(2, testLength, N, N)
 
     prediction_base = multiprocessing.Array(ctypes.c_double, testLength*N*N)
     prediction = np.ctypeslib.as_array(prediction_base.get_obj())
@@ -54,16 +55,14 @@ def parseArguments():
 
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('mode', nargs=1, type=str, help="Can be: NN, RBF, ESN")
-    parser.add_argument('direction', default="vu", nargs=1, type=str, help="vu: v -> u, otherwise: u -> v")
     args = parser.parse_args()
 
-    reverseDirection = args.direction[0] != "vu"
     if args.mode[0] not in ["ESN", "NN", "RBF"]:
         raise ValueError("No valid predictionMode choosen! (Value is now: {0})".format(args.mode[0]))
     else:
         predictionMode = args.mode[0]
 
-    print("Prediction via {0}: {1}".format(predictionMode, "u -> v" if reverseDirection else "v -> u"))
+    print("Prediction via {0}".format(predictionMode))
 parseArguments()
 
 def setupConstants():
@@ -103,7 +102,9 @@ def preparePredicter(y, x):
     if (predictionMode == "ESN"):
         if (y < patch_radius or y >= N-patch_radius or x < patch_radius or x >= N-patch_radius):
             #frame
-            predicter = ESN(n_input = 1, n_output = 1, n_reservoir = n_units,
+            min_border_distance = np.min([y, x, N-1-y, N-1-x])
+
+            predicter = ESN(n_input = int((2*min_border_distance+1)**2), n_output = 1, n_reservoir = n_units,
                     weight_generation = "advanced", leak_rate = 0.70, spectral_radius = 0.8,
                     random_seed=42, noise_level=0.0001, sparseness=.1, regression_parameters=[regression_parameter], solver = "lsqr")
         else:
@@ -121,12 +122,17 @@ def preparePredicter(y, x):
 
     return predicter
 
-def fit_predict_pixel(y, x, running_index, training_data, test_data, predicter):
-    training_data_in = training_data[1][:, y - patch_radius:y + patch_radius+1, x - patch_radius:x + patch_radius+1][:, ::sigma_skip, ::sigma_skip].reshape(-1, eff_sigma*eff_sigma)
-    training_data_out = training_data[0][:, y, x].reshape(-1, 1)
+def prepareFitData(y, x, pr, def_param=(shared_input_data, shared_output_data)):
+    training_data_in = shared_input_data[:trainLength][:, y - pr:y + pr+1, x - pr:x + pr+1][:, ::sigma_skip, ::sigma_skip].reshape(-1, eff_sigma*eff_sigma)
+    test_data_in = shared_input_data[trainLength:][:, y - pr:y + pr+1, x - pr:x + pr+1][:, ::sigma_skip, ::sigma_skip].reshape(-1, eff_sigma*eff_sigma)
 
-    test_data_in = test_data[1][:, y - patch_radius:y + patch_radius+1, x - patch_radius:x + patch_radius+1][:, ::sigma_skip, ::sigma_skip].reshape(-1, eff_sigma*eff_sigma)
-    test_data_out = test_data[0][:, y, x].reshape(-1, 1)
+    training_data_out = shared_output_data[:trainLength][:, y, x].reshape(-1, 1)
+    test_data_out = shared_output_data[trainLength:][:, y, x].reshape(-1, 1)
+
+    return training_data_in, test_data_in, training_data_out, test_data_out
+
+def fit_predict_pixel(y, x, running_index, predicter):
+    training_data_in, test_data_in, training_data_out, test_data_out = prepareFitData(y, x, patch_radius)
 
     predicter.fit(training_data_in, training_data_out)
     pred = predicter.predict(test_data_in)
@@ -134,16 +140,9 @@ def fit_predict_pixel(y, x, running_index, training_data, test_data, predicter):
 
     return pred
 
-def fit_predict_frame_pixel(y, x, running_index, training_data, test_data, predicter):
-    ind_y, ind_x = y, x
-
+def fit_predict_frame_pixel(y, x, running_index, predicter):
     min_border_distance = np.min([y, x, N-1-y, N-1-x])
-
-    training_data_in = training_data[1][:, y - min_border_distance:y + min_border_distance+1, x - min_border_distance:x + min_border_distance+1].reshape(-1, int((2*min_border_distance+1)**2))
-    training_data_out = training_data[0][:, y, x].reshape(-1, 1)
-
-    test_data_in = test_data[1][:, y - min_border_distance:y + min_border_distance+1, x - min_border_distance:x + min_border_distance+1].reshape(-1, int((2*min_border_distance+1)**2))
-    test_data_out = test_data[0][:, y, x].reshape(-1, 1)
+    training_data_in, test_data_in, training_data_out, test_data_out = prepareFitData(y, x, min_border_distance)
 
     predicter.fit(training_data_in, training_data_out)
     pred = predicter.predict(test_data_in)
@@ -154,18 +153,18 @@ def fit_predict_frame_pixel(y, x, running_index, training_data, test_data, predi
 def get_prediction_init(q):
     get_prediction.q = q
 
-def get_prediction(data, def_param=(shared_training_data, shared_test_data)):
+def get_prediction(data):
     y, x, running_index = data
 
     predicter = preparePredicter(y, x)
     pred = None
     if (y >= patch_radius and y < N-patch_radius and x >= patch_radius and x < N-patch_radius):
         #inner point
-        pred = fit_predict_pixel(y, x, running_index, shared_training_data, shared_test_data, predicter)
+        pred = fit_predict_pixel(y, x, running_index, predicter)
 
     else:
         #frame
-        pred = fit_predict_frame_pixel(y, x, running_index, shared_training_data, shared_test_data, predicter)
+        pred = fit_predict_frame_pixel(y, x, running_index, predicter)
 
     get_prediction.q.put((y, x, pred))
 
@@ -205,21 +204,14 @@ def mainFunction():
 
         print("loading finished")
 
-    training_data = data[:trainLength]
-    test_data = data[trainLength:]
-
-    shared_training_data[0, :, :, :] = training_data[:]
-    shared_test_data[0, :, :, :] = test_data[:]
+    shared_output_data[:, :, :] = data[:]
 
     print("blurring...")
 
-    for t in range(trainLength):
-        shared_training_data[1, t, :, :] = gaussian_filter(training_data[t], sigma=9.0)
+    for t in range(ndata):
+        shared_input_data[t, :, :] = gaussian_filter(shared_input_data[t], sigma=9.0)
 
-    for t in range(testLength):
-        shared_test_data[1, t, :, :] = gaussian_filter(test_data[t], sigma=9.0)
-
-    show_results([("Orig", shared_training_data[0]), ("Blurred", shared_training_data[1])])
+    show_results([("Blurred", shared_input_data[:trainLength]), ("Orig", shared_output_data[:trainLength])])
 
     print("blurring finished")
 
@@ -248,12 +240,12 @@ def mainFunction():
     prediction[prediction < 0.0] = 0.0
     prediction[prediction > 1.0] = 1.0
 
-    diff = (test_data-prediction)
+    diff = (shared_output_data[trainLength:]-prediction)
     mse = np.mean((diff)**2)
     print("test error: {0}".format(mse))
     print("inner test error: {0}".format(np.mean((diff[:, patch_radius:N-patch_radius, patch_radius:N-patch_radius])**2)))
 
-    show_results([("Source", shared_test_data[1]), ("Orig", shared_test_data[0]), ("Pred", prediction), ("Diff", diff)])
+    show_results([("Source", shared_input_data[trainLength:]), ("Orig", shared_output_data[trainLength:]), ("Pred", prediction), ("Diff", diff)])
 
 if __name__== '__main__':
     mainFunction()
