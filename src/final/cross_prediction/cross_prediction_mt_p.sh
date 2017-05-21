61c6b22c424363a99b29d86da04dac1c081e16ae
FlashTek <rzrolandzimmermann@live.de>
2017-05-21 18:10:20 +0200

    .

--- src/final/cross_prediction/cross_prediction_mt_p.sh
+++ src/final/cross_prediction/cross_prediction_mt_p.sh
@@ -16,20 +16,20 @@ export PYTHONUNBUFFERED=1
 #$ -j yes
 
 # Standard name of the job (if none is given on the command line):
-#$ -N cross_pred_rbf_vh
+#$ -N cross_pred_rbf_placements_vh
 
 # Path for the output files
-#$ -o /home/roland/q-out_rbf
+#$ -o /home/roland/q-out_cross_rbf_placements
 
 # Limit memory usage
 #$ -hard -l h_vmem=62G
 
 # array range
-#$ -t 91-108
+#$ -t 1-132
 
 # parallel
 #$ -pe mvapich2-grannus05 16
 
 #$ -q mvapich2-grannus05.q
 
-python cross_prediction_mt_p.py RBF vh
+python cross_prediction_mt_p.py RBF2 vh
