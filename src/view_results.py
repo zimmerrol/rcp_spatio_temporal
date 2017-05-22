d8114019395d29888a87581905199450bb1713de
FlashTek <rzrolandzimmermann@live.de>
2017-05-22 20:45:31 +0200

    feature: added newest result sheet and added view_results script

---
+++ src/view_results.py
@@ -0,0 +1,25 @@
+from helper import *
+
+import argparse
+import dill as pickle
+
+parser = argparse.ArgumentParser(description='Shows the visualisation of the results of a 2D field predicted by ML algorithms.')
+parser.add_argument('file', type=str,help='the file of the saved visualisation dictionary', nargs=1)
+parser.add_argument('clim', default=None, nargs="*", type=int)
+args = parser.parse_args()
+
+print("Loading results from '{0}'".format(args.file[0]))
+
+f = open(args.file[0], "rb")
+viewData = pickle.load(f)
+f.close()
+
+clim = args.clim
+if (len(clim) == 0):
+	clim  = None
+else:
+	print("Using clim range {0}".format(clim))
+
+print("Showing results from '{0}'".format(args.file[0]))
+
+show_results(viewData, forced_clim=clim)
