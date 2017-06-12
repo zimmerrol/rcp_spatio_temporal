fad5f548a5af84c5ecdc16435fb63aeb52f0b779
FlashTek <rzrolandzimmermann@live.de>
2017-06-12 13:06:20 +0200

    feature: added ESN parameters for temporal prediction of u

--- src/final/prediction/prediction_p.py
+++ src/final/prediction/prediction_p.py
@@ -12,8 +12,7 @@ def parse_arguments():
     pmtp.id = int(os.getenv("SGE_TASK_ID", 0))
 
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('mode', nargs=1, type=str, help="Can be: ESN")
-    parser.add_argument('direction', default="vu", nargs=1, type=str, help="u (Barkley), v (Mitchell-Schaeffer)")
+    parser.add_argument('direction', default="u", nargs=1, type=str, help="u (Barkley), v (Mitchell-Schaeffer)")
     args = parser.parse_args()
 
     if args.direction[0] not in ["u", "v"]:
@@ -21,37 +20,27 @@ def parse_arguments():
     else:
         pmtp.direction = args.direction[0]
 
-    if args.mode[0] not in ["ESN", "NN", "RBF"]:
-        raise ValueError("No valid predictionMode choosen! (Value is now: {0})".format(args.mode[0]))
-    else:
-        pmtp.predictionMode = args.mode[0]
-
-    print("Prediction via {0}: {1}".format(pmtp.predictionMode, pmtp.direction))
+    print("Prediction: {0}".format(pmtp.direction))
 
 def setup_constants():
     sge_id = pmtp.id
     direction = pmtp.direction
 
     print("Using parameters:")
+    pmtp.n_units = {"u": [200, 200, 400, 300, 200, 200, 200, ], "v": []}[direction][sge_id-1]
+    pmtp.sparseness = {"u": [0.05, 0.2, 0.2, 0.1, 0.1, 0.2, 0.2, ], "v": []}[direction][sge_id-1]
+    pmtp.random_seed = {"u": [39, 40, 42, 39, 40, 39, 41, ], "v": []}[direction][sge_id-1]
 
-    if pmtp.predictionMode == "ESN":
-        pmtp.n_units = {"u": [500], "v": []}[direction][sge_id-1]
-        pmtp.sparseness = {"u": [.05], "v": []}[direction][sge_id-1]
-        pmtp.random_seed = {"u": [41], "v": []}[direction][sge_id-1]
+    pmtp.spectral_radius = {"u": [0.8, 0.8, 0.5, 2.5, 0.95, 1.75, 1.2, ], "v": []}[direction][sge_id-1]
+    pmtp.regression_parameter = {"u": [5.00E-08, 5.00E-07, 0.0005, 5.00E-06, 0.05, 5.00E-05, 0.05, ], "v": []}[direction][sge_id-1]
+    pmtp.leaking_rate = {"u": [0.2, 0.05, 0.05, 0.05, 0.05, 0.05, 0.5, ], "v": []}[direction][sge_id-1]
+    pmtp.noise_level = {"u": [1.00E-05, 0.0001, 1.00E-05, 1.00E-05, 1.00E-05, 0.0001, 1.00E-05, ], "v": []}[direction][sge_id-1]
 
-        pmtp.spectral_radius = {"u": [1.2], "v": []}[direction][sge_id-1]
-        pmtp.regression_parameter = {"u": [3e-8], "v": []}[direction][sge_id-1]
-        pmtp.leaking_rate = {"u": [.95], "v": []}[direction][sge_id-1]
-        pmtp.noise_level = {"u": [.0001], "v": []}[direction][sge_id-1]
+    pmtp.sigma = [1, 3, 5, 5, 7, 7, 7][sge_id-1]
+    pmtp.sigma_skip = [1, 1, 1, 2, 1, 2, 3][sge_id-1]
 
-        pmtp.sigma = [1, 3, 5, 5, 7, 7, 7][sge_id-1]
-        pmtp.sigma_skip = [1, 1, 1, 2, 1, 2, 3][sge_id-1]
-
-        print("\t trainLength \t = {0} \n\t sigma \t = {1}\n\t sigma_skip \t = {2}\n\t n_units \t = {3}\n\t regular. \t = {4}"
-              .format(pmtp.trainLength, pmtp.sigma, pmtp.sigma_skip, pmtp.n_units, pmtp.regression_parameter))
-
-    else:
-        raise ValueError("No valid predictionMode choosen! (Value is now: {0})".format(pmtp.predictionMode))
+    print("\t trainLength \t = {0} \n\t sigma \t = {1}\n\t sigma_skip \t = {2}\n\t n_units \t = {3}\n\t regular. \t = {4}"
+          .format(pmtp.trainLength, pmtp.sigma, pmtp.sigma_skip, pmtp.n_units, pmtp.regression_parameter))
 
     pmtp.eff_sigma = int(np.ceil(pmtp.sigma/pmtp.sigma_skip))
     pmtp.patch_radius = pmtp.sigma//2
