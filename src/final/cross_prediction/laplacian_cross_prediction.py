31853bd83aaefb077f7a679fea8cc860e826b47a
FlashTek <rzrolandzimmermann@live.de>
2017-06-18 16:10:46 +0200

    .

--- src/final/cross_prediction/laplacian_cross_prediction.py
+++ src/final/cross_prediction/laplacian_cross_prediction.py
@@ -123,13 +123,13 @@ def generate_data(N, Ngrid):
     return data
 
 print("loading data...")
-#input_data, output_data = generate_data(ndata, Ngrid=N)
+input_data, output_data = generate_data(ndata, Ngrid=N)
 
 print("reshaping data...")
-#input_data = input_data[:trainLength+predictionLength]
-#output_data = output_data[:trainLength+predictionLength]
-#input_data_f = input_data.reshape((ndata, -1))
-#output_data_f = output_data.reshape((ndata, -1))
+input_data = input_data[:trainLength+predictionLength]
+output_data = output_data[:trainLength+predictionLength]
+input_data_f = input_data.reshape((ndata, -1))
+output_data_f = output_data.reshape((ndata, -1))
 
 print("setting up...")
 predicter = ESN(n_input=n_units, n_output=n_units, n_reservoir=n_units,
