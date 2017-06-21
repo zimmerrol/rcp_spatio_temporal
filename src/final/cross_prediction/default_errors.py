a1c0f8172bc8139e4b0ebf212b515a0f8766944d
FlashTek <rzrolandzimmermann@live.de>
2017-06-21 08:54:46 +0200

    .

--- src/final/cross_prediction/default_errors.py
+++ src/final/cross_prediction/default_errors.py
@@ -18,35 +18,54 @@ ntest = 2000
 data = None
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('direction', default="u", nargs=1, type=str, help="uv: u -> v, vu: v -> u, vh: v -> h, hv: h -> v")
+parser.add_argument('direction', default="vu", nargs=1, type=str, help="vu: v -> u, uv: u -> v, hv: h -> v, vh: v -> h, bocf_uv: BOCF u -> v, bocf_uw: BOCF u -> w, bocf_us: BOCF u -> s")
 args = parser.parse_args()
 
-if args.direction[0] not in ["uv", "vu", "vh", "hv"]:
+if args.direction[0] not in ["vu", "uv", "hv", "vh", "bocf_uv", "bocf_uw", "bocf_us"]:
     raise ValueError("No valid direction choosen! (Value is now: {0})".format(args.direction[0]))
 else:
     direction = args.direction[0]
 
-if (direction in ["uv", "vu"]):
-    if not os.path.exists("../../cache/barkley/raw/{0}_{1}.uv.dat.npy".format(ndata, N)):
-        data = bh.generate_uv_data(ndata, 20000, 5, Ngrid=N)
-        np.save("../../cache/barkley/raw/{0}_{1}.uv.dat.npy".format(ndata, N), data)
+if direction in ["uv", "vu"]:
+    if not os.path.exists("../../cache/barkley/raw/{0}_{1}.uv.dat.npy".format(N, Ngrid)):
+        data = bh.generate_uv_data(N, 50000, 5, Ngrid=Ngrid)
+        np.save("../../cache/barkley/raw/{0}_{1}.uv.dat.npy".format(N, Ngrid), data)
     else:
-        data = np.load("../../cache/barkley/raw/{0}_{1}.uv.dat.npy".format(ndata, N))
+        data = np.load("../../cache/barkley/raw/{0}_{1}.uv.dat.npy".format(N, Ngrid))
+elif direction in ["bocf_uv", "bocf_uw", "bocf_us"]:
+    if not os.path.exists("../../cache/bocf/raw/{0}_{1}.uvws.dat.npy".format(N, Ngrid)):
+        data = bocfh.generate_uvws_data(N, 50000, 50, Ngrid=Ngrid)
+        np.save("../../cache/bocf/raw/{0}_{1}.uvws.dat.npy".format(N, Ngrid), data)
+    else:
+        data = np.load("../../cache/bocf/raw/{0}_{1}.uvws.dat.npy".format(N, Ngrid))
 else:
-    if not os.path.exists("../../cache/mitchell/raw/{0}_{1}.vh.dat.npy".format(ndata, N)):
-        data = mh.generate_vh_data(ndata, 20000, 50, Ngrid=N)
-        np.save("../../cache/mitchell/raw/{0}_{1}.vh.dat.npy".format(ndata, N), data)
+    if not os.path.exists("../../cache/mitchell/raw/{0}_{1}.vh.dat.npy".format(N, Ngrid)):
+        data = mh.generate_vh_data(N, 20000, 50, Ngrid=Ngrid)
+        np.save("../../cache/mitchell/raw/{0}_{1}.vh.dat.npy".format(N, Ngrid), data)
     else:
-        data = np.load("../../cache/mitchell/raw/{0}_{1}.vh.dat.npy".format(ndata, N))
+        data = np.load("../../cache/mitchell/raw/{0}_{1}.vh.dat.npy".format(N, Ngrid))
 
 print("Data loaded")
 
-#at the moment we are doing a u -> v / v -> h cross prediction.
-if direction in ["vu", "hv"]:
+#at the moment we are doing a u -> v / v -> h cross prediction (index 0 -> index 1)
+if (direction in ["vu", "hv"]):
     #switch the entries for the v -> u / h -> v prediction
     tmp = data[0].copy()
     data[0] = data[1].copy()
     data[1] = tmp.copy()
+
+if direction in ["bocf_uv", "bocf_uw", "bocf_ws"]:
+    real_data = np.empty((2, N, Ngrid, Ngrid))
+    real_data[0] = data[0].copy()
+
+    if direction == "bocf_uv":
+        real_data[1] = data[1].copy()
+    elif direction == "bocf_uw":
+        real_data[1] = data[2].copy()
+    elif direction == "bocf_us":
+        real_data[1] = data[3].copy()
+
+    data = real_data
 
 training_data = data[:, :ntrain]
 test_data = data[:, ntrain + nvalidation:ntrain + nvalidation + ntest]
